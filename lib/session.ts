e80ddd76965a645d1d88f4d510b6af3d4c799077
HaruLife <9105dud@gmail.com>
2025-04-30 21:10:17 +0900

    test

--- lib/session.ts
+++ lib/session.ts
@@ -9,5 +9,9 @@ export default function getSession() {
   return getIronSession<SessionContent>(cookies(), {
     cookieName: "delicious-karrot",
     password: process.env.COOKIE_PASSWORD!,
+    cookieOptions: {
+      // maxAge: 60 * 60 * 24, // 24시간 = 86400초
+      maxAge: undefined,
+    },
   });
 }
